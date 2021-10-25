50cd5a852e73749b5bfdf32efdd8113279615111
Gulbike Biyekenova <gbknva@gmail.com>
2021-10-25 21:41:01 +0000

    Implement head

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    return console.log(`⛔⛔⛔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`✅ ✅ ✅  Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+const head = function(firstItem) {
+  return firstItem[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
