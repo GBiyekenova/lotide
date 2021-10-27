325c989813ea4515683c20e6b4febc129415061d
Gulbike Biyekenova <gbknva@gmail.com>
2021-10-27 01:14:13 +0000

    complete assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+// const eqArrays = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
+// return true;
+// };
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return console.log(`Assertion failed: ${arr1} !== ${arr2}`);
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return console.log(`Assertion failed: ${arr1} !== ${arr2}`);
+    }
+  }
+  console.log(`Assertion passed: ${arr1} === ${arr2}`);
+};
+assertArraysEqual([1, 1, 1], [1, 1]);
