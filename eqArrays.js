1779f43e229a2ba021faeb22ad84cf74d498a675
Gulbike Biyekenova <gbknva@gmail.com>
2021-10-27 00:33:36 +0000

    solve the assignment

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -15,4 +18,4 @@ const eqArrays = function(array1, array2) {
 return true;
 };
 
-assertEqual(eqArrays([1,2], [1,2]), true); // => should PASS
+assertEqual(eqArrays([1,2,undefined], [1,2]), true); // => should PASS
