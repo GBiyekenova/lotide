2998d32643a922199abcad67ad834d029db5ef0e
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-02 02:07:12 +0000

    refactor to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
-  it('returns 3 for 3', () => {
+  it('returns 3 for length of ["Yo Yo", "Lighthouse", "Labs"]', () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     tail(words);
     assert.strictEqual(words.length, 3);
